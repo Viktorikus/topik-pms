360d59ab6da55f102b06e9fd714d69c71ec6a887
SirSmothie <victorrikus@gmail.com>
2025-06-06 22:51:58 +0700

    firts commit

---
+++ config/cors.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'], // Ganti dengan domain frontend di production
+    'allowed_headers' => ['*'],
+];
